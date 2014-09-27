0d3b3cb9b982e282d6e225d94e851972d4d98f83
David Young <ohnoitsyou7043@gmail.com>
2014-09-27 18:02:30 -0400

    making the arduino software happy, reading serial
    Had to move the master.ino file into its own folder to make the arduino
    software happy.
    Finally got the arduino to read serial data properly and I can now parse
    it for commands

--- master/master.ino
+++ master/master.ino
@@ -15,16 +15,36 @@ void setup(void) {
   // print Preamble
   Serial.begin(57600);
   printf_begin();
-  
-  
+
+
   // Setup the radio
-  radio.begin()
+  radio.begin();
   // setup retries
   radio.setRetries(15,15);
   //payload size
   radio.setPayloadSize(8);
-  
+
   // Setup 2 pipes
   radio.openWritingPipe(pipes[0]);
   radio.openReadingPipe(1,pipes[1]);
-  
+
+  radio.printDetails();
+}
+
+void loop(void) {
+  while(Serial.available() > 0) {
+    int red = Serial.parseInt();
+    int grn = Serial.parseInt();
+    int blu = Serial.parseInt();
+  }   
+  if(Serial.read() == '\n') {
+    red = 255 - constrain(red,0,255);
+    grn = 255 - constrain(grn,0,255);
+    blu = 255 - constrain(blu,0,255);
+    Serial.print(red);
+    Serial.print(":");
+    Serial.print(grn);
+    Serial.print(":");
+    Serial.print(blu);
+  }
+}
